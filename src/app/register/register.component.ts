4284e7e4ed0b283978bfdf6608fe2b0110bb760b
ChristopheThiou <christophe.thiou.simplon@gmail.com>
2023-07-11 13:26:34 +0200

    update

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -5,14 +5,13 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  
-  user:User = {email: '', password: '', role: '' }
+  user: User = { email: '', password: '', role: '' };
   constructor(private service: AuthService) {}
 
   onSubmit(user: User) {
-    this.service.addUser(user).subscribe(data => this.user);
+    this.service.addUser(user).subscribe();
   }
 }
